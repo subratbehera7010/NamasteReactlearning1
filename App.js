f807378a9fb2b526cc16768603a40b2a7965319a
Subrat Behera <subratbehera7010@gmail.com>
2024-08-21 15:23:05 -0500

    NamasteReactLearnings

---
+++ App.js
@@ -0,0 +1,14 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hi, I'm h1 tag"),
+    React.createElement("h2", {}, "Hi, I'm h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Hi, I'm h1 tag"),
+    React.createElement("h2", {}, "Hi, I'm h2 tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
